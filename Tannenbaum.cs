c39ecb61d30327d14e32eaac0fa85e76263b916d
edp-nandog <ngmuender@edp.ch>
2022-01-20 16:23:07 +0100

    tannenbaum

--- Tannenbaum.cs
+++ Tannenbaum.cs
@@ -12,9 +12,33 @@ namespace Tannenbaum
 {
     public partial class Form1 : Form
     {
+
+        int baumbreite;
+        int baumhoehe;
+        int kronenhoehe;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baumbreite = Convert.ToInt32(textBox1);
+            baumhoehe = Convert.ToInt32(textBox2);
+            kronenhoehe = Convert.ToInt32(textBox3);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
         }
     }
 }
